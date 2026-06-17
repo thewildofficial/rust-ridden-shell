de8ef794037b9ca7878ea0fe4ecbdc141923c203
thewildofficial <abanstampy@gmail.com>
2026-06-17 18:00:39 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,12 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    // handle user input here using read_line
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+
+    // print message not found
+    // xyz: command not found  
+    println!("{}: command not found", command.trim());
 }
