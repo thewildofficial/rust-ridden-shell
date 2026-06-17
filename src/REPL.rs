a3d4b20dca5f1b351faad4e42734c773f72456ff
thewildofficial <abanstampy@gmail.com>
2026-06-17 18:09:31 +0530

    codecrafters submit [skip ci]

---
+++ src/REPL.rs
@@ -0,0 +1,65 @@
+/*
+In this stage, you'll implement a REPL (Read-Eval-Print Loop).
+The REPL
+
+A REPL (Read-Eval-Print Loop) is an interactive loop that forms the core of a shell. It follows a repeating cycle:
+
+    Read: Display a prompt and wait for user input
+    Eval: Parse and execute the command
+    Print: Display the output or error message
+    Loop: Return to step 1 and wait for the next command
+
+This cycle continues indefinitely until the shell process is terminated.
+
+Your shell should follow this same cycle:
+
+    Display the prompt $, then wait for a line of input.
+    Print <command_name>: command not found for any command the user enters, like with the previous stages.
+    Return to step 1.
+
+For example, if the user types hello, your shell should print hello: command not found, then display the prompt ($) again.
+Tests
+
+The tester will execute your program like this:
+
+./your_program.sh
+
+It will then send a series of commands to your shell:
+
+$ invalid_command_1
+invalid_command_1: command not found
+$ invalid_command_2
+invalid_command_2: command not found
+$ invalid_command_3
+invalid_command_3: command not found
+$
+
+After each command, the tester will verify that your shell:
+
+    Prints the message <command_name>: command not found.
+    Displays a new prompt ($) before the tester sends the next command
+
+Notes
+
+    The exact number of commands sent and the command names will be random.
+    The loop should run indefinitely. The tester will terminate your program when the test is complete.
+*/
+fn repl() {
+    // main event loop
+    loop {
+        // print the prompt
+        print!("$ ");
+        // flush the output to ensure the prompt is displayed
+        std::io::stdout().flush().unwrap();
+        // read user input
+        let mut command = String::new();
+        std::io::stdin().read_line(&mut command).unwrap();
+
+        // check if the trimmed command is empty (user just pressed enter)
+        if command.trim().is_empty() {
+            continue; // skip to the next iteration of the loop
+        }
+        // print command not found message
+        println!("{}: command not found", command.trim());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,6 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-    // handle user input here using read_line
-    let mut command = String::new();
-    io::stdin().read_line(&mut command).unwrap();
-
-    // print message not found
-    // xyz: command not found  
-    println!("{}: command not found", command.trim());
+    // call the main REPL function
+    repl();
 }
